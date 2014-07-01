781b8e9394cfc84d740d508229ccc7f630c0d0af
Ali Paikan <ali.paikan@iit.it>
2014-07-01 16:22:48 +0200

    updated  simulator script

--- scripts/prepare_simulator.sh
+++ scripts/prepare_simulator.sh
@@ -5,7 +5,8 @@ echo "set pos 0 -40" | yarp write ...  /icubSim/head/rpc:i
 
 #clear the worls
 echo "world del all" | yarp write ... /icubSim/world
-
+sleep 0.2
 # add objects to the world
 echo "world mk model milk_box.x milk_box.bmp 0.1 0.7 0.4" | yarp write ... /icubSim/world 
-
+sleep 1
+echo "world mk model nesquik.x nesquik.bmp -0.05 0.7 0.35" | yarp write ... /icubSim/world 
